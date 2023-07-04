d54958e93b4b9a50b841da86f9e9d7b4e57e63a4
LuCeHe <manucelotti@gmail.com>
2023-07-04 18:27:50 +0200

    minor changes

--- neural_models_pt/__init__.py
+++ neural_models_pt/__init__.py
@@ -1,8 +1,6 @@
-from anthe_official.neural_models_pt.transformer import *
-from anthe_official.neural_models_pt.special_embedding import *
-from anthe_official.neural_models_pt.helper_layers import *
-from anthe_official.neural_models_pt.attentions import *
-from anthe_official.neural_models_pt.tensor_chain.dense import *
-from anthe_official.neural_models_pt.tensor_chain.embedding import *
-from anthe_official.neural_models_pt.tensor_chain.convolutions import *
+from anthe_official.neural_models_pt.transformer import Transformer
+from anthe_official.neural_models_pt.special_embedding import select_embedding_type, HSoftPOS, SoftPOS, EmbeddingLayer
+from anthe_official.neural_models_pt.helper_layers import ProjectionLayer
+from anthe_official.neural_models_pt.attentions import MultiHeadAttention
+from anthe_official.neural_models_pt.tensor_chain.dense import TCDense
 
